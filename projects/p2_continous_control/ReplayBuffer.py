9c7049d8e4fb0fb290dbd193291e2310ae14fa61
steffen <steffen.koerner@mytum.de>
2022-01-19 23:27:57 +0100

    Add Gaussian Noise Function

--- projects/p2_continous_control/ReplayBuffer.py
+++ projects/p2_continous_control/ReplayBuffer.py
@@ -44,3 +44,40 @@ class ReplayBuffer:
     def __len__(self):
         """Return the current size of internal memory."""
         return len(self.memory)
+
+
+class GaussianNoise:
+    def __init__(self, size, std_start, std_end=None, steps=None):
+        if std_end is None:
+            std_end = std_start
+            steps = 1
+        self.inc = (std_end - std_start) / float(steps)
+        self.current_std = std_start
+        self.std_end = std_end
+        self.std_start = std_start
+        self.size = size
+
+    def __call__(self):
+        self.current_std = np.clip(self.current_std - self.inc, self.std_end, self.std_start)
+        return np.random.normal(0,self.current_std, self.size)
+
+
+
+class GaussianProcess():
+    def __init__(self, size, std):
+        self.size = size
+        self.std = std
+
+    def sample(self):
+        return np.random.randn(*self.size) * self.std()
+
+
+
+class LinearIncrements:
+    def __init__(self, start, end=None, steps=None):
+        if end is None:
+            end = start
+            steps = 1
+        self.inc = (end - start) / float(steps)
+        self.current = start
+        self.end = end
