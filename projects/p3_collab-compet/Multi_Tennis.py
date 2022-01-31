37e15cf65cfeeecfa71dc77157bc4f00d779f4ac
steffen <steffen.koerner@mytum.de>
2022-01-31 18:27:53 +0100

    Updat Mutliplayer

--- projects/p3_collab-compet/Multi_Tennis.py
+++ projects/p3_collab-compet/Multi_Tennis.py
@@ -44,7 +44,7 @@ class ActorNetwork(nn.Module):
     def forward(self, state):
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
-        x = F.tanh(self.fc3(x))
+        x = torch.tanh(self.fc3(x))
         return x
 
 class DDPGNetwork():
@@ -59,15 +59,14 @@ class DDPGNetwork():
         return self.actor_network(state)
     
     def critic(self, states,actions):
-        sta = states[0]
-        sta = states[1]
-        return self.critic_network(torch.cat((states[0], states[1] ,actions[0], actions[1]), 1))
+        result = torch.cat((states[0], states[1] ,actions[0], actions[1]), 1)
+        return self.critic_network(result)
 
 
 
 
 class DDPGAgent():
-    def __init__(self, state_size, action_size, seed,critic_input_size, warmup = 100):
+    def __init__(self, state_size, action_size, seed,critic_input_size, warmup):
         self.state_size = state_size
         self.action_size = action_size
         self.seed = random.seed(seed)
@@ -89,7 +88,7 @@ class DDPGAgent():
             actions = np.random.randn(self.action_size) 
             actions = np.clip(actions, self.action_lowest_value, self.action_highest_value)   
         else:         
-            state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+            state = torch.from_numpy(state).float().to(device)
             self.local_network.actor_network.eval()
             with torch.no_grad():
                 actions = self.local_network.actor(state).cpu().data.numpy()
@@ -98,31 +97,6 @@ class DDPGAgent():
             actions = np.clip(actions + noise_applied, self.action_lowest_value, self.action_highest_value)
         return actions
 
-
-    def learn(self, gamma):
-        """Update parameters.
-        """
-        if len(self.memory) > BATCH_SIZE:
-            experiences = self.memory.sample()
-            states, actions, rewards, next_states, dones = experiences
-
-            y = rewards + (1 - dones) * gamma * self.target_network.critic(next_states, self.target_network.actor(next_states))
-            critic_loss = F.mse_loss(y , self.local_network.critic(states,actions))
-            self.local_network.critic_network.zero_grad()
-            critic_loss.backward()
-            self.local_network.critic_optimizer.step()
-
-            actor_loss = self.local_network.critic(states.detach(), self.local_network.actor(states))
-            actor_loss = -actor_loss.mean()
-            self.local_network.actor_network.zero_grad()
-            actor_loss.backward()
-            self.local_network.actor_optimizer.step()
-
-            self.soft_update(self.local_network, self.target_network, TAU) 
-
-    # def step(self,state, action, reward, next_state, done, gamma):
-    #     self.memory.add(state, action, reward, next_state, done)
-    #     self.learn(gamma)
 
     def soft_update(self, tau):
         """Soft update model parameters for actor and critic of target network.
@@ -142,54 +116,46 @@ class DDPGAgent():
 
 
 class MADDPGAgent():
-    def __init__(self, state_size, action_size, seed, warmup = 100):
+    def __init__(self, state_size, action_size, seed, warmup):
         self.number_agents = 2
         self.critic_input_size=state_size *self.number_agents+ self.number_agents*action_size
-        self.agents = [DDPGAgent(state_size=state_size,action_size=action_size,critic_input_size=self.critic_input_size, seed=seed) for i in range(0,self.number_agents)]
+        self.agents = [DDPGAgent(state_size=state_size,action_size=action_size,critic_input_size=self.critic_input_size, seed=seed, warmup=warmup) for i in range(0,self.number_agents)]
         self.memory = ReplayBuffer(action_size=action_size, buffer_size=BUFFER_SIZE, batch_size=BATCH_SIZE, seed=seed)   
 
     def get_acion_per_current_policy_for(self, observation_all , number_episode, noise=0):
+        assert len(observation_all) == len(self.agents)
         actions = [agent.get_acion_per_current_policy_for(obs,number_episode) for agent,obs in zip(self.agents, observation_all)]
-        return actions
+        result = np.array(actions)
+        assert result.ndim == 2
+        return result
 
-    def step(self,state, action, reward, next_state, done, gamma):
+    def step(self,state, action, reward, next_state, done, gamma,i_episode,episode_step):
         self.memory.add(state, action, reward, next_state, done)
-        self.learn(gamma)
+        self.learn(gamma,i_episode,episode_step)
     
-    def learn(self, gamma):
+    def learn(self, gamma,i_episode,episode_step):
         if len(self.memory) > BATCH_SIZE:
-            experiences = self.memory.sample()
-
-            states, actions, rewards, next_states, dones = experiences
-
-
             for i in range(0,2):
-                state = states[i]
-                action = actions[i]
-                reward = rewards[i]
-                next_state = next_states[i]
-                done = dones[i]
+                experiences = self.memory.sample()
+                states, actions, rewards, next_states, dones = experiences
 
-                y = reward + (1 - done) * gamma * self.agents[i].target_network.critic(next_states, [self.agents[0].target_network.actor(next_states[0]),self.agents[1].target_network.actor(next_states[1])])
+                ## TODO: Somwehow the sizes don't seem to match
+                q_next = self.agents[i].target_network.critic(next_states, [self.agents[0].target_network.actor(next_states[0]),self.agents[1].target_network.actor(next_states[1])])
+                y = rewards[i] + (1 - dones[i]) * gamma * q_next
                 critic_loss = F.mse_loss(y , self.agents[i].local_network.critic(states,actions)) 
                 self.agents[i].local_network.critic_network.zero_grad()
                 critic_loss.backward()
                 self.agents[i].local_network.critic_optimizer.step()
+                #writer.add_scalar("multi/train/critic_loss" +str(i), critic_loss, i_episode)
 
-            ######################### old stuff #######  
-            # y = rewards + (1 - dones) * gamma * self.target_network.critic(next_states, self.target_network.actor(next_states))
-            # critic_loss = F.mse_loss(y , self.local_network.critic(states,actions))
-            # self.local_network.critic_network.zero_grad()
-            # critic_loss.backward()
-            # self.local_network.critic_optimizer.step()
+                actor_loss = self.agents[i].local_network.critic(states.detach(), [self.agents[0].local_network.actor(states[0]), self.agents[1].local_network.actor(states[1])])
+                actor_loss = -actor_loss.mean()
+                self.agents[i].local_network.actor_network.zero_grad()
+                actor_loss.backward()
+                self.agents[i].local_network.actor_optimizer.step()
+                #writer.add_scalar("multi/train/actor_loss" + str(i), -actor_loss, i_episode)
 
-            # actor_loss = self.local_network.critic(states.detach(), self.local_network.actor(states))
-            # actor_loss = -actor_loss.mean()
-            # self.local_network.actor_network.zero_grad()
-            # actor_loss.backward()
-            # self.local_network.actor_optimizer.step()
-
-            # self.soft_update(self.local_network, self.target_network, TAU) 
+                self.agents[i].soft_update(TAU) 
 
     def soft_update(self, local_model, target_model, tau):
         for agent in self.agents:
@@ -213,32 +179,31 @@ def mddpg(env, agent, n_episodes=2000, max_t=1000, gamma=0.9):
     max_score_value = 0
     brain_name = env.brain_names[0]
     brain = env.brains[brain_name]
-    noise = 2
+    noise = 1
     noise_decay = 0.9999
     for i_episode in range(1, n_episodes+1):
         env_info = env.reset(train_mode=True)[brain_name] 
         state = env_info.vector_observations
-        score = 0
+        score = [0,0]
         noise *= noise_decay
+        episode_step = 0
         while True:
+            episode_step += 1
             action = agent.get_acion_per_current_policy_for(state, i_episode, noise)
-            # action1 = agent1.get_acion_per_current_policy_for(state[0], i_episode, noise)
-            # action2 = agent2.get_acion_per_current_policy_for(state[1], i_episode, noise)
-            # action = [action1,action2]
             env_info = env.step(action)[brain_name]
             next_state = env_info.vector_observations
             reward = env_info.rewards
             done = env_info.local_done
-            agent.step(state, action,reward,next_state,done, gamma)
-            # agent1.step(state[0], action1, reward[0], next_state[0], done[0], gamma)
-            # agent2.step(state[1], action2, reward[1], next_state[1], done[1], gamma)
+
+            agent.step(state, action,reward,next_state,done, gamma,i_episode,episode_step)
             state = next_state
-            score += np.max(reward)
+            score += reward
             if done[0] or  done[1]:
                 break 
-        writer.add_scalar("multi/train/score", score, i_episode)
-        scores_window.append(score)
-        scores.append(score)
+        max_score = np.max(score)
+        writer.add_scalar("multi/train/score", max_score, i_episode)
+        scores_window.append(max_score)
+        scores.append(max_score)
         if i_episode % 200 == 0:
             #plot_scores(scores,i_episode)
             pass
@@ -246,19 +211,19 @@ def mddpg(env, agent, n_episodes=2000, max_t=1000, gamma=0.9):
         print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
         if i_episode % 100 == 0:
             print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
-        # if np.mean(scores_window) > max_score_value + 0.1:
-        #     print('\nEnvironment saved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
-        #     torch.save(agent1.local_network.actor_network.state_dict(), 'multi_intermediate_weight_actor1.pth')
-        #     torch.save(agent1.local_network.critic_network.state_dict(), 'multi_intermediate_weight_critic1.pth')
-        #     torch.save(agent2.local_network.actor_network.state_dict(), 'multi_intermediate_weight_actor2.pth')
-        #     torch.save(agent2.local_network.critic_network.state_dict(), 'multi_intermediate_weight_critic2.pth')
-        #     max_score_value = np.mean(scores_window)
-        # if np.mean(scores_window) >= 30:
-        #     print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
-        #     torch.save(agent1.local_network.actor_network.state_dict(), 'multi_final_weight_actor1.pth')
-        #     torch.save(agent1.local_network.critic_network.state_dict(), 'multi_final_weight_critic1.pth')
-        #     torch.save(agent2.local_network.actor_network.state_dict(), 'multi_final_weight_actor2.pth')
-        #     torch.save(agent2.local_network.critic_network.state_dict(), 'multi_final_weight_critic2.pth')
+        if np.mean(scores_window) > max_score_value + 0.1:
+            print('\nEnvironment saved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.agents[0].local_network.actor_network.state_dict(), 'multi_intermediate_weight_actor1.pth')
+            torch.save(agent.agents[0].local_network.critic_network.state_dict(), 'multi_intermediate_weight_critic1.pth')
+            torch.save(agent.agents[1].local_network.actor_network.state_dict(), 'multi_intermediate_weight_actor2.pth')
+            torch.save(agent.agents[1].local_network.critic_network.state_dict(), 'multi_intermediate_weight_critic2.pth')
+            max_score_value = np.mean(scores_window)
+        if np.mean(scores_window) >= 30:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.agents[0].local_network.actor_network.state_dict(), 'multi_final_weight_actor1.pth')
+            torch.save(agent.agents[0].local_network.critic_network.state_dict(), 'multi_final_weight_critic1.pth')
+            torch.save(agent.agents[1].local_network.actor_network.state_dict(), 'multi_final_weight_actor2.pth')
+            torch.save(agent.agents[1].local_network.critic_network.state_dict(), 'multi_final_weight_critic2.pth')
         #     break
     return scores
 
@@ -277,7 +242,7 @@ if __name__ == '__main__':
     state_size = states.shape[1]
 
 
-    agent = MADDPGAgent(state_size= state_size, action_size = action_size, seed = 0 , warmup = 200)
+    agent = MADDPGAgent(state_size= state_size, action_size = action_size, seed = 0 , warmup = 0)
     scores = mddpg(env,agent, n_episodes=5000000000)
 
     writer.flush()
