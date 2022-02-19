5c4836c79befaebb915c6ac9891131f260ab3c1e
steffen <steffen.koerner@mytum.de>
2022-02-19 11:42:48 +0100

    Move more DNN params into Config

--- library/configs/dqn_config.py
+++ library/configs/dqn_config.py
@@ -1,6 +1,9 @@
-
+import torch.nn as nn
+from collections import OrderedDict
 class Config:
   def __init__(self):
+
+    self.seed = 0
     self.replay_buffer_size = int(1e5)
     self.batch_size = 64
     self.gamma = 0.99
@@ -11,6 +14,7 @@ class Config:
     self.eps_start = 1.0
     self.eps_end = 0.01
     self.eps_decay = 0.995
+    #self.noise =
 
     self.stop_return = 13
     self.save_each_return_step = 3
@@ -18,3 +22,10 @@ class Config:
     #TODO Move config for Neural Network to here
     self.input_layer_size = 37
     self.output_layer_size = 4
+    self.model = nn.Sequential(
+          nn.Linear(self.input_layer_size,64),
+          nn.ReLU(),
+          nn.Linear(64,64),
+          nn.ReLU(),
+          nn.Linear(64, self.output_layer_size),
+        )
