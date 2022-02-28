220ce534338347981098370817de09af726d22a2
steffen <steffen.koerner@mytum.de>
2022-02-28 18:13:04 +0100

    Update Main

--- library/examples/banana_env/main.py
+++ library/examples/banana_env/main.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-m", "--mode", help = "The mode should be either train or eval")   
     args = parser.parse_args()
-    env = UnityEnvironment(file_name="/home/steffen/workspace/PushBlockCustom/PushBlockCustom.x86_64")
+    env = UnityEnvironment(file_name="/home/steffen/workspace/PushBlockNonDevelopment/PushBlockCustom.x86_64")
 
     env.reset()
     behavior_name = list(env.behavior_specs)[0]
@@ -34,38 +34,6 @@ if __name__ == "__main__":
       print(f"There are {spec.action_spec.continuous_size} continuous actions")
     if spec.action_spec.is_discrete():
       print(f"There are {spec.action_spec.discrete_size} discrete actions")
-
-
-    # How many actions are possible ?
-
-    # For discrete actions only : How many different options does each action has ?
-    # if spec.action_spec.discrete_size > 0:
-    #   for action, branch_size in enumerate(spec.action_spec.discrete_branches):
-    #     print(f"Action number {action} has {branch_size} different options")
-
-    # decision_steps, terminal_steps = env.get_steps(behavior_name)
-
-    # action = spec.action_spec.random_action(len(decision_steps))
-    # env.set_actions(behavior_name, action)
-    # Perform a step in the simulation
-
-    
-    # ###### old stuff
-    # brain_name = env.brain_names[0]
-    # brain = env.brains[brain_name]
-    # # number of agents in the environment
-    # print('Number of agents:', len(env.agents))
-
-    # # number of actions
-    # action_size = brain.vector_action_space_size
-    # print('Number of actions:', action_size)
-
-    # # examine the state space 
-    # state = env_info.vector_observations[0]
-    # print('States look like:', state)
-    # state_size = len(state)
-    # print('States have length:', state_size)
-
 
 
     config = Config()
