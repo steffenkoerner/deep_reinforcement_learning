f32e3beda5451e3622f83c5f5f4827e32bdb20a3
steffen <steffen.koerner@mytum.de>
2022-02-28 18:11:55 +0100

    Update DQN Unity

--- library/algorithms/dqn_unity.py
+++ library/algorithms/dqn_unity.py
@@ -10,13 +10,10 @@ def dqn_unity(env,agent, config):
     scores_window = deque(maxlen=100)
     eps = config.eps_start
     agent_id = 0
-    # brain_name = env.brain_names[0]
     for i_episode in range(1, config.number_episodes+1):
-        # env_info = env.reset(train_mode=True)[brain_name]
         env.reset()
         behavior_name = list(env.behavior_specs)[0]
         spec = env.behavior_specs[behavior_name]
-        # state = env_info.vector_observations[0]
         decision_steps, terminal_steps = env.get_steps(behavior_name)
         agent_index = decision_steps.agent_id_to_index[agent_id]
         state = decision_steps.obs[0][agent_index]
@@ -25,14 +22,10 @@ def dqn_unity(env,agent, config):
         for episode_length in range(1, config.episode_length):
             action = agent.act(state, eps)  
             action_reshaped = action.reshape((1,1))
-            # action_tuple = spec.action_spec.random_action(len(decision_steps))
-            # bla = action_tuple.discrete
             action_tuple = ActionTuple(discrete=action_reshaped)
             env.set_actions(behavior_name,action_tuple)
             env.step()
             decision_steps, terminal_steps = env.get_steps(behavior_name)
-
-            a = len(decision_steps)
             if agent_id in decision_steps.agent_id:
                 agent_index = decision_steps.agent_id_to_index[agent_id]
                 reward = decision_steps.reward[agent_index]
@@ -45,9 +38,8 @@ def dqn_unity(env,agent, config):
             else:
                 print("Something is wrong !!!")
                 break
-            # done = env_info.local_done[0]  
 
-            agent.step(state, action_tuple.discrete, reward, next_state, done)
+            agent.step(state, action, reward, next_state, done)
             state = next_state
             score += reward
             if done:
@@ -56,6 +48,8 @@ def dqn_unity(env,agent, config):
         scores.append(score)
         eps = max(config.eps_end, config.eps_decay*eps)
         mean_value = np.mean(scores_window)
+        config.logger.add_scalar("score", score, i_episode)    
+        config.logger.add_scalar("mean scores", mean_value, i_episode)   
         log_and_save(agent,i_episode,mean_value, config)
         if mean_value >= config.stop_return:
             break
